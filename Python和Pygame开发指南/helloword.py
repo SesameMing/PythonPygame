dc3c07d3f49b6280a58db28064b17e688402b2d0
SemaseMing <admin@v-api.cn>
2016-08-26 19:45:54 +0800

    holleword

---
+++ Python和Pygame开发指南/helloword.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+# Author:SemaseMing <blog.v-api.cn>
+# Time:2016-08-26 18:27
+
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((400, 300))
+pygame.display.set_caption("Holle World!")
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
